daa710bcf82f5199508659e70e3f7387d1431cf3
Luis Eduardo Mendez Tafolla <luismendeztaf3@gmail.com>
2024-05-13 12:13:40 -0600

    prueba de libreria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { File } from '@awesome-cordova-plugins/file/ngx';
+import { MediaCapture } from '@awesome-cordova-plugins/media-capture/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },File,MediaCapture],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { MediaCapture } from 'media-capture';
-
+import { MediaCapture,CaptureImageOptions} from '@awesome-cordova-plugins/media-capture/ngx';
+// import { MediaCapture } from 'media-capture';
+import { File } from '@awesome-cordova-plugins/file/ngx';
 
 @Component({
   selector: 'app-home',
@@ -9,18 +10,37 @@ import { MediaCapture } from 'media-capture';
 })
 export class HomePage {
 
+  video:any;
+  constructor(private mediaCapture:MediaCapture, private file: File){
+
+  }
   async captureVideo() {
     try {
-      const result = await MediaCapture['captureVideo']({
-        duration: 300,
-        quality: 'hd',
-        sizeLimit: 0
-      });
-
-      console.log('Vídeo capturado:', result.file);
+      let options: CaptureImageOptions = { limit: 1 }
+      const data:any = await this.mediaCapture.captureVideo(options);
+      this.video = data[0];
+      console.log(this.video);
+      let dir = this.video.localURL.split('/');
+      dir.pop();
+      let fromDir = dir.join('/');
+      let toDir = this.file.dataDirectory;
+      const response = await this.file.copyFile(fromDir, this.video.name, toDir, this.video.name);
+      console.log(response);
     } catch (error) {
-      console.error('Erro ao capturar vídeo:', error);
+      console.log(error);
+      
     }
+    // try {
+    //   const result = await MediaCapture['captureVideo']({
+    //     duration: 300,
+    //     quality: 'hd',
+    //     sizeLimit: 0
+    //   });
+
+    //   console.log('Vídeo capturado:', result.file);
+    // } catch (error) {
+    //   console.error('Erro ao capturar vídeo:', error);
+    // }
   }
   
 
